5e974a964e2f4c8f6cd2c85325e994c43a7ecc4f
Ashish jha <ajha5645@gmail.com>
2026-07-08 15:42:17 +0530

    Initial Spring Boot project

---
+++ Server/naruto-card-clash-server/src/main/java/com/ashish/narutocardclash/NarutoCardClashServerApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.narutocardclash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoCardClashServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarutoCardClashServerApplication.class, args);
+    }
+
+}
